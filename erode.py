76357b76fe2e587524f068bb81bf91ea8095c6c1
francisca1 <fprebolledo@uc.cl>
2020-11-16 22:26:11 -0300

    fix area limit

--- erode.py
+++ erode.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 num = 24306
 
 for i in range(50):
-    img = cv2.imread(f'images/ISIC_00{num+6}.jpg')
+    img = cv2.imread(f'images/ISIC_00{num+i}.jpg')
     median = cv2.medianBlur(img,21)    
     grey = cv2.cvtColor(median, cv2.COLOR_BGR2GRAY)
 
@@ -19,22 +19,26 @@ for i in range(50):
         kernel = np.ones((j,j),np.uint8)
         binary = cv2.morphologyEx(binary,cv2.MORPH_OPEN,kernel)
         binary = cv2.morphologyEx(binary,cv2.MORPH_CLOSE,kernel)
-
+        # quiza no hacer estos tan grandes, si no que hacerlos chicos, hasta 6 y luego cerrar las ciurvas que se puedan.
    
     contours,hierarchy = cv2.findContours(binary, 1, 2)
     plt.imshow(binary)
     plt.show()
+    prom = 0
+    areas = list(map(cv2.contourArea, contours))
+    other= 255-binary
+    
     for cont in contours:
         area = cv2.contourArea(cont)
-        if area < 4000:
-            print("entre")
+        prom += area
+        if area < max(areas):
             mask = np.zeros(grey.shape,np.uint8)
             cv2.drawContours(mask,[cont],0,255,-1)
             pixelpoints = np.transpose(np.nonzero(mask))
             for pint in pixelpoints:
                 binary[pint[0]][pint[1]] = 0
-        else:
-            print(area)
+
+    print(prom/len(contours))
     plt.imshow(binary)
     plt.show()
     break
