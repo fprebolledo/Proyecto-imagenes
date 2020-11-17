ea2ff23873d5072612fc02d5a9b4257bd777a9be
francisca1 <fprebolledo@uc.cl>
2020-11-16 23:02:56 -0300

    ajuste

--- adaptative.py
+++ adaptative.py
@@ -4,6 +4,23 @@ from matplotlib import pyplot as plt
 
 num = 24306
 
+def delete_min_areas(binary_img):
+    contours, _ = cv2.findContours(binary_img, 1, 2)
+    # creo una lista con las áreas de los contornos
+    areas = list(map(cv2.contourArea, contours))
+    i = 0
+    # los itero y si el area es menor que el area maxima lo eliimino
+    for cont in contours:
+        area = areas[i]
+        if area < max(areas):
+            #mascara de unos
+            mask = np.zeros(binary_img.shape,np.uint8)
+            cv2.drawContours(mask,[cont],0,255,-1)
+            pixelpoints = np.transpose(np.nonzero(mask))
+            for pint in pixelpoints:
+                binary_img[pint[0]][pint[1]] = 0
+        i += 1
+
 for i in range(50):
     img = cv2.imread(f'images/ISIC_00{num+i}.jpg')
     median = cv2.medianBlur(img,21)    
@@ -20,26 +37,13 @@ for i in range(50):
         binary = cv2.morphologyEx(binary,cv2.MORPH_OPEN,kernel)
         binary = cv2.morphologyEx(binary,cv2.MORPH_CLOSE,kernel)
         # quiza no hacer estos tan grandes, si no que hacerlos chicos, hasta 6 y luego cerrar las ciurvas que se puedan.
-   
-    contours,hierarchy = cv2.findContours(binary, 1, 2)
-    #plt.imshow(binary)
-    #plt.show()
-    prom = 0
-    areas = list(map(cv2.contourArea, contours))
-    other= 255-binary
     
-    for cont in contours:
-        area = cv2.contourArea(cont)
-        prom += area
-        if area < max(areas):
-            mask = np.zeros(grey.shape,np.uint8)
-            cv2.drawContours(mask,[cont],0,255,-1)
-            pixelpoints = np.transpose(np.nonzero(mask))
-            for pint in pixelpoints:
-                binary[pint[0]][pint[1]] = 0
 
-    #print(prom/len(contours))
-    #plt.imshow(binary)
-    #plt.show()
+    delete_min_areas(binary)
+    ohter = 255-binary
+    delete_min_areas(ohter)
+    binary = 255-ohter
+
+    # plt.imshow(binary)
+    # plt.show()
     cv2.imwrite(f"results/IMG{num+i}A.jpg",binary)
-    #generalizar el área 
