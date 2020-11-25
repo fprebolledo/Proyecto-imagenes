0a77aac2c178d88aa31262616db0f4d02321c16b
francisca1 <fprebolledo@uc.cl>
2020-11-24 23:30:35 -0300

    arreglo funcion de TPR FPR

--- results.py
+++ results.py
@@ -9,11 +9,17 @@ def print_img(img, nombre):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+TOTAL_T, TOTAL_F = 0,0
 for i in range(50):
     real = cv2.imread(f'images/ISIC_00{num+i}_segmentation.png', 0)
     obtenido = cv2.imread(f'results/IMG{num+i}A.jpg', 0)
+    # no entiendo porque los bordes cuando se guarda la imagen quedan con 1 o 254 y hay que hacer esto uwu
+    _, obtenido = cv2.threshold(obtenido,127,255,cv2.THRESH_BINARY)
     true_labels, pred_labels = real.flatten(), obtenido.flatten()
-    print_img(obtenido, "sskd")
+
+    ## todas estas funciones devuelven un array de trues y falses si el valor coincide y luego se hace el and entre esto
+    # como el and devuelve un 1 si ambos son iguales y un 0 si no, la suma nos da el total.
+
     # True Positive (TP): we predict a label of 1 (positive), and the true label is 1.
     TP = np.sum(np.logical_and(pred_labels == 255, true_labels == 255))
     
@@ -25,4 +31,9 @@ for i in range(50):
     
     # False Negative (FN): we predict a label of 0 (negative), but the true label is 1.
     FN = np.sum(np.logical_and(pred_labels == 0, true_labels == 255))
-    print(TP, TN, FP, FN, sum([TP, TN, FP, FN]))
+    TPR = TP/(TP+FN)
+    FPR = FP/(FP+TN)
+    TOTAL_T += TPR
+    TOTAL_F += FPR
+
+print("TASA TPR: ", TOTAL_T/50,"TASA FPR: ", TOTAL_F/50)
