1ba7598b8d8f48f4527857d5208b31a21ede5ee5
José Lara <jvlara@uc.cl>
2020-12-08 14:42:46 -0300

    fix: results

--- results.py
+++ results.py
@@ -13,6 +13,7 @@ def calculate_results(tipo):
     
     TOTAL_T, TOTAL_F, TOTAL_F1 = 0, 0, 0
     total_tpr = []
+    total_f1 = []
     total_fpr = []
     bad_tpr = []
     bad_fpr = []
@@ -58,9 +59,13 @@ def calculate_results(tipo):
         TOTAL_F1 += F1
         total_tpr.append(TPR)
         total_fpr.append(FPR)
+        total_f1.append(F1)
     
     total_tpr = np.array(total_tpr)
     total_fpr = np.array(total_fpr)
+    # total_f1 = np.array(total_f1)
+    # best_f1 = np.argsort(-total_tpr)[0]
+    # worst_f1 = np.argsort(total_tpr)[0]
     worst_tpr = np.argsort(total_tpr)[:8] ## el - es para que sean los menores (los n menores tpr)
     worst_fpr = np.argsort(-total_fpr)[:8] ## sin el - es para que sean los mayores (los n mayores fpr)
     TOTAL_T_RES = np.round((TOTAL_T/50) *100, 3)
@@ -72,13 +77,17 @@ def calculate_results(tipo):
     BAD_T_RES = np.round(BAD_T*100/len(bad_imgs), 3)
     BAD_F_RES = np.round(BAD_F*100/len(bad_imgs), 3)
     BAD_F1_RES = np.round(BAD_F1*100/len(bad_imgs), 3)
-    print(f"-----------------RESULTADOS {tipo}-------------")
-    print("-------------- IMAGENES FÁCILES -----------------")
-    print("TASA TPR: ", GOOD_T_RES, "TASA FPR: ", GOOD_F_RES)
-    print("-------------- IMAGENES DIFICILES -----------------")
-    print("TASA TPR: ", BAD_T_RES, "TASA FPR: ", BAD_F_RES)
-    print("-------------------- TOTAL -----------------------")
-    print("TASA TPR: ", TOTAL_T_RES, "TASA FPR: ", TOTAL_F_RES)
+    # print(f"-----------------RESULTADOS {tipo}-------------")
+    # print("-------------- IMAGENES FÁCILES -----------------")
+    # print("TASA TPR: ", GOOD_T_RES, "TASA FPR: ", GOOD_F_RES)
+    # print("-------------- IMAGENES DIFICILES -----------------")
+    # print("TASA TPR: ", BAD_T_RES, "TASA FPR: ", BAD_F_RES)
+    # print("-------------------- TOTAL -----------------------")
+    # print("TASA TPR: ", TOTAL_T_RES, "TASA FPR: ", TOTAL_F_RES)
+    # print(best_f1)
+    # print(f"{total_f1[best_f1]}")
+    # print(worst_f1)
+    # print(f"{total_f1[worst_f1]}")
     return TOTAL_T_RES, TOTAL_F_RES, TOTAL_F1_RES, BAD_T_RES, BAD_F_RES, BAD_F1_RES, GOOD_T_RES, GOOD_F_RES, GOOD_F1_RES, worst_tpr, worst_fpr
 
 
@@ -109,7 +118,8 @@ def resultados_csv(tipos, output_name):
     
 if __name__ == "__main__":
     # Tienen que estar todos los tipos, si no, tira error al no encontrar imagen.
-    tipos = ["A", "O", "EQ", "EQO", "GMMS", "GMMSA","OS", "H", "W", "RW", "kmeansRGB1", "kmeansHSV2", "kmeansLAB2"]
+    # tipos = ["A", "O", "EQ", "EQO", "GMMS", "GMMSA","OS", "H", "W", "RW", "kmeansRGB1", "kmeansHSV2", "kmeansLAB2"]
+    tipos = ["GMMSA"]
     # tipos = ["kmeansRGB1", "kmeansHSV2", "kmeansLAB2"]
     resultados_csv(tipos, "resultados.csv")
     # Para todos correr todos los kmeans.
